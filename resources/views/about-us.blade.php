c3e5ec79cb748be6e0ec4246d966231de7e175e9
Zsolt148 <b.zsolt148@gmail.com>
2022-04-08 14:48:18 +0200

    Google maps

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -9,19 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-5 pb-10 sm:px-10 bg-white border-b border-gray-200">
-                    <div class="text-4xl mb-8">Ki vagyok én ?</div>
+                    <div class="text-4xl my-8">Kik vagyunk ?</div>
                     <div class="mb-5">
                         A nevem Budai Zsolt, 1999-ben születtem Kecskeméten és azóta is itt élek. 4 éve kezdtem el
                         foglalkozni webfejlesztéssel. Idén megalapítottam az egyéni vállalkozásomat mint webfejlesztő.
                     </div>
-                    <div>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    </div>
-                    <div class="text-4xl my-8">Kik vagyunk ?</div>
                     <div>
                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -39,7 +31,10 @@
         </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-5 sm:mt-10">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/x-LXYG-0eXM" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                <iframe width="100%" height="400" src="https://www.youtube.com/embed/x-LXYG-0eXM" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-5 sm:mt-10">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12967.641650603597!2d19.683027216437278!3d46.900538405877676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dcfc212e97f7%3A0x9b9e696fe3194912!2sAZ-Land%20Kft.%20-%20BestByte%20Plus!5e0!3m2!1shu!2shu!4v1649414895535!5m2!1shu!2shu" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
 
