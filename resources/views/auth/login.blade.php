ef745a6812aedaa072bb35800166663ce32ff84c
Zsolt148 <b.zsolt148@gmail.com>
2022-05-11 13:41:14 +0200

    Login and register translations

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,14 +17,14 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" value="Email cím" />
+                <x-label for="email" value="Email" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" value="Jelszó" />
+                <x-label for="password" value="Password" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -36,16 +36,16 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">Emlékezz rám</span>
+                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    Regisztráció
+                    Register
                 </a>
                 <x-button class="ml-3">
-                   Bejelentkezés
+                   Login
                 </x-button>
             </div>
         </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,19 +14,19 @@
 
             <!-- Name -->
             <div>
-                <x-label for="name" value="Név" />
+                <x-label for="name" value="name" />
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
-                <x-label for="email" value="Email cím" />
+                <x-label for="email" value="Email" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" value="Jelszó" />
+                <x-label for="password" value="Password" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -36,7 +36,7 @@
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-label for="password_confirmation" value="Jelszó mégegyszer" />
+                <x-label for="password_confirmation" value="Password again" />
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
@@ -45,11 +45,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    Bejelentkezés
+                    Login
                 </a>
 
                 <x-button class="ml-4">
-                    Regisztráció
+                    Register
                 </x-button>
             </div>
         </form>
